c8ea4ea8f8a97cee536a763d5a838950ff904a1d
jgcapdev <jorge.gomez-carrillo-external@capgemini.com>
2022-08-10 09:16:22 +0200

    feat: fetch pokemons an retrieve from Redux

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { initPokemons } from '../redux/actions/pokemons/actions.js';
 import pokemonService from '../services/pokemons.js';
 
@@ -7,19 +7,47 @@ import BaseTitle from './UI/BaseTitle.js';
 
 const PokemonsList = () => {
   const dispatch = useDispatch();
+  const pokemons = useSelector((state) => state.pokemons);
 
   useEffect(() => {
-    pokemonService.getAll().then(
-      (pokemons) => {
+    if (pokemons.length === 0) {
+      pokemonService.getAll().then((pokemons) => {
         dispatch(initPokemons(pokemons.results));
-      },
-      [dispatch]
-    );
-  });
+      });
+    }
+  }, [dispatch, pokemons.length]);
 
   return (
     <div>
       <BaseTitle title="Pokemons List" />
+
+      <div className="mx-24">
+        <table className="table-auto text-center w-full">
+          <thead className="border-b">
+            <tr>
+              <th className="text-gray-900 px-6 py-4">Pokemon</th>
+              <th className="text-gray-900 px-6 py-4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemons.map((pokemon) => {
+              return (
+                <tr className="py-4 border-b" key={pokemon.name}>
+                  <td className="py-2">{pokemon.name}</td>
+                  <td className="py-2">
+                    <button
+                      type="button"
+                      className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      Add
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
